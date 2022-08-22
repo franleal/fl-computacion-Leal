366a81cdf859b62a071eeb93cd37284301d1160c
Francisco Leal <franciscoleal152000@gmail.com>
2022-08-22 12:41:28 -0300

    agregado de navBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+export const Header = () =>{
+    return(
+        <header className="Header">
+            <h1 className="Title">FL-Computacion</h1>
+
+            <nav className="Nav">
+                <a className="Links">productos</a>
+                <a className="Links">Ubicacion</a>
+                <a className="Links">PC armada</a>
+            </nav>
+        </header>
+    )
+}
